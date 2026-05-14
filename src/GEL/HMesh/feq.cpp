6404425eb6f7e0125c7dfb4ebba38acbe8a57c20
Thor Dyrbjerg Vestergaard Christiansen <tdvc@mac.comp.clients.local>
2026-05-14 18:40:22 +0200

    Added feq.cpp and feq.h to GEL

---
+++ src/GEL/HMesh/feq.cpp
@@ -0,0 +1,3 @@
+#include "feq.h"
+
+
---
+++ src/GEL/HMesh/feq.h
@@ -0,0 +1,20 @@
+/* ----------------------------------------------------------------------- *
+ * This file is part of GEL, http://www.imm.dtu.dk/GEL
+ * Copyright (C) the authors and DTU Informatics
+ * For license and list of authors, see ../../doc/intro.pdf
+ * ----------------------------------------------------------------------- */
+
+/**
+ * @file feq.h
+ * @brief Compute the decomposition of a FEQ mesh
+ */
+
+
+#ifndef GEL_FEQ_H
+#define GEL_FEQ_H
+
+#include <iostream>
+#include <fstream>
+
+
+#endif
