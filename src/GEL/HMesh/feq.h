b19f51ba57c663908b022dc293451be202a8d3b7
Thor Dyrbjerg Vestergaard Christiansen <tdvc@mac.comp.clients.local>
2026-05-14 19:38:01 +0200

    Added small comment to feq.h

--- src/GEL/HMesh/feq.h
+++ src/GEL/HMesh/feq.h
@@ -2,6 +2,7 @@
  * This file is part of GEL, http://www.imm.dtu.dk/GEL
  * Copyright (C) the authors and DTU Informatics
  * For license and list of authors, see ../../doc/intro.pdf
+ * Made by Thor Christiansen
  * ----------------------------------------------------------------------- */
 
 /**
